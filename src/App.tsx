a1f158c5cfc4dbac8ba835c1ae356f1719422e24
Jose Claudio <jcsj.ti@gmail.com>
2024-05-09 16:32:12 -0300

    setup shadcn and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Button variant="default">Click me</Button>
+    </div>
+  );
 }
 
 export default App;
